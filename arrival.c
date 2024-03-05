01368540478220339acfb9480f57eb279e2effc4
g610g <gio.gonzales@carsu.edu.ph>
2024-03-05 23:41:17 +0800

    all nodes zero arrival issue fix

--- arrival.c
+++ arrival.c
@@ -61,12 +61,11 @@ void print_current_nodes(Process** nodes, int length){
     {
         printf("Burst time: %d id: %d\n", nodes[i]->burst_time, nodes[i]->id);
     }
-    
 }
-int get_next_arrival_time(int current_time, Process* head){
+int get_next_arrival_time(int* current_time, Process* head){
     Process* temp = head;
     while(temp != NULL){
-        if (temp->arrival_time != current_time){
+        if (temp->arrival_time != (*current_time)){
             return temp->arrival_time;
         }
         temp = temp->next;
@@ -75,13 +74,15 @@ int get_next_arrival_time(int current_time, Process* head){
 Process* update_list(Process* head, int id_arr_length, int* ids){
     Process* temp = head, *next = NULL;
     int i = 0;
-    while(i < id_arr_length || temp != NULL){
+    // printf("ids len:%d\n", id_arr_length);
+    while(i < id_arr_length && temp != NULL){
         //if the to be deleted is the head of the list
         if (temp == head && ids[i] == temp->id){
             head = temp->next;
             temp->next->prev = NULL;
             free(temp);
-            temp = head->next;
+            temp = head;
+            printf("head update\n");
             i++;
         }else if(ids[i] == temp->id){
             temp->next->prev = temp->prev;
@@ -90,53 +91,63 @@ Process* update_list(Process* head, int id_arr_length, int* ids){
             free(temp);
             temp = next;
             i++;
-        
         }else{
             temp = temp->next;
         }
     }
+    // print_list(head);
     return head;
 }
 //we have to get the next arrival time of this
-int process_nodes_array(Process** nodes, int length, int current_time, int next_time, int* ids, int id_index){
+int process_nodes_array(Process** nodes, int length, int* current_time, int next_time, int* ids, int id_index){
+    //might be the last or all nodes have the same arrival time
+    if (next_time == 0){
+        next_time = nodes[length - 1]->burst_time;
+    }
     for (int i = 0; i < length; i++)
     {
-        int difference = next_time - current_time;
+        int difference = next_time - (*current_time);
         //might be the head of the list 
         //head might be updated
         if (nodes[i]->burst_time <= next_time){
-            if (difference > nodes[i]->burst_time){
-                current_time = nodes[i]->burst_time;
+            if (difference >= nodes[i]->burst_time){
+                (*current_time) += nodes[i]->burst_time;
                 //push the id to be deleted in the list
                 ids[id_index] = nodes[i]->id;
-                id_index++;
+                id_index += 1;
                 continue;
-            }else if (difference < nodes[i]->burst_time){
-                //updates only in the node
+            }else if (difference == 0){
+                
+            }
+            else if (difference < nodes[i]->burst_time){
+                //updates only in the node no need to put in array
                 nodes[i]->burst_time -= difference;
-                current_time = next_time;
+                (*current_time) = next_time;
                 //load again into the array
             }
 
         }
     }
+    (*current_time) = next_time;
     return id_index;
+
 }
 
 //load the task that has the same time into the array
-Process* load_array(Process*head, int arrival_time){
+Process* load_array(Process*head, int* arrival_time){
     Process** nodes = malloc(sizeof(Process*) * MAX);
     int ids[MAX];
     Process* temp = head;
     int i = 0;
     while (temp != NULL){
-        if (temp->arrival_time == arrival_time){
+        
+        if (temp->arrival_time == (*arrival_time) || temp->arrival_time < (*arrival_time)){
             nodes[i] = temp;
             i++;
         }
         temp = temp->next;
     }
-    print_current_nodes(nodes, i);
+    // print_current_nodes(nodes, i);
     int id_arr_length = process_nodes_array(nodes, i, arrival_time, get_next_arrival_time(arrival_time, head), ids, 0);
     head = update_list(head, id_arr_length, ids);
     free(nodes);
@@ -148,13 +159,13 @@ Process* load_array(Process*head, int arrival_time){
 int main(void){
     Process* processes = NULL;
     int arrival = 0;
-    processes = initialize(processes, 5);
-    load_array(processes, arrival);
+    processes = initialize(processes, 2);
+    processes = load_array(processes, &arrival);
+
     // while (processes != NULL)
     // {
-    //     load_array(processes, arrival);
+    //     processes = load_array(processes, &arrival);
     // }
-    // print_current_nodes(nodes, &start,&end);
     // print_list(processes);
     int current_process_index = 0;
 
